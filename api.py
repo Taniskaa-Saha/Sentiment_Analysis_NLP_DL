196791e05b9af976a97202813301251b9a5d8919
Taniskaa-Saha <tan.b7478@gmail.com>
2026-09-17 16:50:39 +0530

    updated

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app=FastAPI()
 
@@ -28,3 +29,28 @@ emotion_emojis = {
     'sadness': '😢',
     'surprise': '😲'
 }
+
+#preprocess upcomming text
+import re
+def preprocess_text(text:str)-> str:
+    #lowercase the text
+    text=text.lower()
+    text=re.sub(r"'", "", text)
+    text=re.sub(r"[^a-z0-9\s]", " ", text)
+    text=re.sub(r"\s+", " ", text).strip()
+    return text
+
+class TextRequest(BaseModel):
+    text: str = Field(..., 
+                      min_length=1,
+                      max_length=2000,
+                      description="The text to analyze for sentiment",
+                      json_schema_extra={
+                          "example": "I am so happy today!"}
+                          )
+
+class PredictionResponse(BaseModel):
+    text: str
+    predicted_emotion: str
+    confidence: float
+    all_probabilities: dict[str, float]
