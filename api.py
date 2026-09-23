2542f18996b9b3bbba560fd43aa6e9f4c15465fd
Taniskaa-Saha <tan.b7478@gmail.com>
2026-09-23 20:35:39 +0530

    complete api file

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
-from api import FastAPI, StaticFiles, HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
 from keras.models import load_model
 from contextlib import asynccontextmanager
@@ -6,30 +7,33 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
+from pathlib import Path
+import numpy as np
 import re
 import pickle
 
 
 #model path
-model_path="ARTIFACTS/BiGRU_model.keras"
+BASE_DIR = Path(__file__).resolve().parent
+model_path = BASE_DIR / "ARTIFACTS" / "BiGRU_model.keras"
 
 #Tokenizer path
-tokenizer_path="ARTIFACTS/tokenizer.pkl"
+tokenizer_path = BASE_DIR / "ARTIFACTS" / "tokenizer.pkl"
 
 #MaxSequence Length
 max_sequence_length = 50
 
 #emotion classes
-emotion_classes = ['anger', 'fear', 'joy', 'love', 'sadness', 'surprise']
+emotion_classes = ['sadness', 'anger', 'love', 'surprise', 'fear', 'joy']
 
 #emotion emojis
 emotion_emojis = {
+    'sadness': '😢',
     'anger': '😠',
+    'love': '❤️',
+    'surprise': '😲',
     'fear': '😨',
     'joy': '😄',
-    'love': '❤️',
-    'sadness': '😢',
-    'surprise': '😲'
 }
 
 #preprocess upcomming text
@@ -70,14 +74,16 @@ async def lifespan(app:FastAPI):
     #global model, tokenizer
     dl_model["BiGRU"] = load_model(model_path)   #BiGRU model
     with open(tokenizer_path, "rb") as file:
-        dl_model["Tokenizer "] = pickle.load(file)
+        dl_model["Tokenizer"] = pickle.load(file)
     print("Model and tokenizer loaded.")
 
     yield #pause, model Is loaded and softer is running at this point model waits for request.
     dl_model.clear()  #clear the model and tokenizer from memory when server shuts down
 
 #mount static files to  FAST api
-app=FastAPI()
+app=FastAPI(
+    lifespan=lifespan,
+)
 
 #enable CORS (Cross-Origin Resource Sharing) to allow requests from any origin
 app.add_middleware(
@@ -88,12 +94,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount('/static', StaticFiles(directory='static'), name='static')
+#app.mount('/static', StaticFiles(directory='static'), name='static')
+app.mount('/static', StaticFiles(directory=BASE_DIR / "static"), name='static')
 
 #API endpoints
 @app.get('/', include_in_schema=False)
 def server_ui():
-    return FileResponse('static/index.html')
+    return FileResponse(BASE_DIR / "static" / "index.html")
 
 @app.get('/health', response_model=HealthResponse)
 def health_check():
@@ -102,14 +109,31 @@ def health_check():
 @app.post('/predict', response_model=PredictionResponse)
 def predict_emotion(input_data: TextInput):
     BiGRU_model = dl_model.get("BiGRU")
-    tokenizer = dl_model.get("Tokenizer ")
+    tokenizer = dl_model.get("Tokenizer")
 
     if BiGRU_model is None or tokenizer is None:
         raise HTTPException(status_code=503, detail="Model or tokenizer not loaded. Please try again later.")
 
     cleaned_text = preprocess_text(input_data.text)
     tokenized_text = tokenizer.texts_to_sequences([cleaned_text])
-    padded_text = pad_sequences(tokenized_text, maxlen=max_sequence_length, padding='post', truncating='post')
+    print(cleaned_text)
+    padded_text = pad_sequences(
+        tokenized_text, 
+        maxlen=max_sequence_length, 
+        padding='post', 
+        truncating='post'
+        )
 
     probabilities = BiGRU_model.predict(padded_text)[0]
-    all_probabilities = {emotion: float(prob) for emotion, prob in zip(emotion_classes, probabilities)}
+    top_emotion_index = int(np.argmax(probabilities))
+    all_probabilities = {
+        label: float(prob) 
+        for prob, label in zip(probabilities, emotion_classes)
+    }
+
+    return PredictionResponse(
+        text=input_data.text,
+        predicted_emotion=emotion_classes[top_emotion_index],
+        confidence=float(probabilities[top_emotion_index]),
+        all_probabilities=all_probabilities
+    )
